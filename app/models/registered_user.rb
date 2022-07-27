fcfccb5654e85f577523e6e0b61a2f57ea5e48ea
Allie Hollenbaugh <aghollenbaugh@gmail.com>
2022-07-26 18:40:08 -0700

    Add fields to registration new/edit pages

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -5,6 +5,7 @@ class RegisteredUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  :first_name, :last_name, :state, :city, :address, :phone_number
   # attr_accessible :title, :body
 end
